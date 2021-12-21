b3d63d2a5eceeb46dd62a008856a4de5138da206
hamid <hbeigi57@gmail.com>
2021-12-21 13:49:24 +0330

    compelete header and navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
 $(document).ready(function () {
-    $(".list-style-one").click(function (e) { 
+    $(".list-style-one").click(function (e) {
         let navList = document.querySelectorAll(".list-style-one");
 
-        navList.forEach(function (item) { $(item).removeClass("active") });
+        navList.forEach(function (item) {
+            $(item).removeClass("active")
+        });
 
         $(this).addClass("active");
-        
+
+
+    });
+
+    $("#small_screen_nav_toggler").click(function (e) {
+        e.stopPropagation();
+        $("#sm_screen_navbar").addClass("nav-show");
+        $(".overlay").addClass("show-overlay");
+
+    });
+
+    $("#off_closer").click(function (e) {
+        e.stopPropagation();
+        $("#sm_screen_navbar").removeClass("nav-show");
+        $(".overlay").removeClass("show-overlay");
+    });
+    $(".overlay").click(function (e) {
+        e.stopPropagation();
+        $("#sm_screen_navbar").removeClass("nav-show");
+        $(".overlay").removeClass("show-overlay");
     });
 });
